28ce0186480192fd833a822476db9901e9238ddf
Bruce wei <father.bruce.wei@gmail.com>
2017-01-03 22:00:03 -0800

    init project

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Greeter {
+    static int add (int x, int y) {
+        return x + y;
+    }
+
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
