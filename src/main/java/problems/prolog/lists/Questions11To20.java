5ff443576771fabafc27bc4bced05bc93152a499
Bruce Wei <bruce.wei@nike.com>
2017-01-06 14:08:00 -0800

    update to question 15

--- src/main/java/problems/prolog/lists/Questions11To20.java
+++ src/main/java/problems/prolog/lists/Questions11To20.java
@@ -37,7 +37,6 @@ public class Questions11To20<T> {
      *    Question 12
      *    Decode a run-length encoded list
      */
-
     public Function<List<Tuple<Integer, T>>, List<T>> decode =
             x ->  x.flatMap(e -> this.decodeTuple.apply(e));
 
@@ -45,7 +44,7 @@ public class Questions11To20<T> {
     private Function<Tuple<Integer, T>, List<T>> decodeTuple =
             x ->  this.multiple.apply(x._1, x._2);
 
-
+    // not tail safe. but good enough for this small question
     private BiFunction<Integer, T, List<T>> multiple =
             (i, x) -> i == 0 ?
                     NIL :
@@ -53,6 +52,40 @@ public class Questions11To20<T> {
 
 
 
+    /*
+     *    Question 13
+     *    Run-length encoding of a list (direct solution).
+     */
+    public Function<List<T>, List<Tuple<Integer, T>>> encodeDirect =
+            ls -> ls.foldRight(
+                NIL,
+                (T x, List<Tuple<Integer, T>> acc) -> acc.isEmpty()?
+                                        list(new Tuple<>(1, x)):
+                                            acc.head()._2.equals(x) ?
+                                                list(new Tuple<>(acc.head()._1  + 1, x)).concat(acc.tail()):
+                                                list(new Tuple<>(1, x)).concat(acc)
+            );
+
+
+    /*
+     *    Question 14
+     *    Duplicate the elements of a list
+     */
+    Function<List<T>, List<T>> duplicate =
+            ls -> ls.flatMap(
+                x -> list(x, x)
+            );
+
+
+    /*
+     *    Question 15
+     *    Duplicate the elements of a list a given number of times.
+     */
+    BiFunction<Integer,List<T>, List<T>> duplicateN =
+            (n, ls) -> ls.flatMap(
+                  x->
+                    this.multiple.apply(n, x)
+            );
 
 
 }
--- src/test/java/problems/prolog/lists/TestQuestions11To20.java
+++ src/test/java/problems/prolog/lists/TestQuestions11To20.java
@@ -24,4 +24,25 @@ public class TestQuestions11To20 {
         assertEquals("[a, a, a, b, b, Nil]", testInstance.decode.apply(list(new Tuple<>(3, 'a'), new Tuple<>(2, 'b'))).toString());
     }
 
+    //Run-length encoding of a list (direct solution).
+    @Test
+    public void testQuestion13(){
+        assertEquals("[(2, a), (3, b), (1, c), Nil]", testInstance.encodeDirect.apply(list('a', 'a', 'b', 'b', 'b', 'c')).toString());
+
+    }
+
+    //Duplicate the elements of a list
+    @Test
+    public void testQuestion14(){
+        assertEquals("[a, a, b, b, c, c, c, c, d, d, Nil]", testInstance.duplicate.apply(list('a', 'b', 'c', 'c', 'd')).toString());
+    }
+
+
+    //Duplicate the elements of a list a given number of times.
+    @Test
+    public void testQuestion15(){
+        assertEquals("[a, a, a, b, b, b, c, c, c, c, c, c, d, d, d, Nil]",  testInstance.duplicateN.apply(3, list('a', 'b', 'c', 'c', 'd')).toString());
+    }
+
+
 }
